0e1cb89a0878580655324a866afb3996476e86ac
mdsiamss10 <mdsiamss10@gmail.com>
2022-07-03 13:03:58 +0600

    Hello

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import express from "express";
 const app = express();
-const port = 9000;
+const port = 3000;
 app.use("/",(red,res)=>{
     res.json({message:"Hello World"});
 });
-app.listen(9000,()=>{
+app.listen(3000,()=>{
     console.log("Starting server on port " + port);
 });
